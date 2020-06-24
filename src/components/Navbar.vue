7e364fc5516755163d3dfdb07be5e873c9d0c63a
Xednom <monde.lacanlalay@gmail.com>
2020-06-24 19:34:32 +0800

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Budget Monitoring</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+          <router-link class="nav-link" :to="{name: 'Home'}">Home</router-link>
+      </li>
+      <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'About'}">About</router-link>
+      </li>
+      <li class="nav-item dropdown ml-auto">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-user"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/accounts/logout/">Logout</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+.dropdown-menu {
+    right: 0;
+    left: auto;
+}
+</style>
